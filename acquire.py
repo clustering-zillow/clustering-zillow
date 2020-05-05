4bef4e1025325f6bcb636c68cd683d9e7402365c
brandireger <brandi.reger@gmail.com>
2020-05-05 17:03:16 -0500

    added acquire.py

---
+++ acquire.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from env import get_db_url
+
+sql = """
+SELECT prop.*, 
+       pred.logerror, 
+       pred.transactiondate, 
+       air.airconditioningdesc, 
+       arch.architecturalstyledesc, 
+       build.buildingclassdesc, 
+       heat.heatingorsystemdesc, 
+       landuse.propertylandusedesc, 
+       story.storydesc, 
+       construct.typeconstructiondesc 
+FROM   properties_2017 prop  
+       INNER JOIN (SELECT parcelid,
+       					  logerror,
+                          Max(transactiondate) transactiondate 
+                   FROM   predictions_2017 
+                   GROUP  BY parcelid, logerror) pred
+               USING (parcelid) 
+       LEFT JOIN airconditioningtype air USING (airconditioningtypeid) 
+       LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid) 
+       LEFT JOIN buildingclasstype build USING (buildingclasstypeid) 
+       LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid) 
+       LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid) 
+       LEFT JOIN storytype story USING (storytypeid) 
+       LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid) 
+WHERE  prop.latitude IS NOT NULL 
+       AND prop.longitude IS NOT NULL
+"""
+
+
+def get_zillow(sql):
+    url = get_db_url('zillow')
+    zillow_df = pd.read_sql(sql, url, index_col='id')
+    return zillow_df
+
+

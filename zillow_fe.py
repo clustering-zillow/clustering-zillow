077fed881f61db8d4ba9446028ba1d46b8bea4c9
brandireger <brandi.reger@gmail.com>
2020-05-08 12:28:30 -0500

    edited model file

--- zillow_fe.py
+++ zillow_fe.py
@@ -40,13 +40,17 @@ def calculated_features(df):
 def bin_features(df):
     df['sf_bin'] = pd.cut(df.finishedsquarefeet12, 
                               bins = [0, 800, 1100, 1400, 1800, 2200, 10000],
-                              labels = [0, 800, 1100, 1400, 1800, 2200])
+                              labels = ['0 to 800', '800 to 1100', 
+                                        '1100 to 1400', '1400 to 1800', 
+                                        '1800 to 2200', '2200 to 10,000'])
     df['age_bin'] = pd.cut(df.age, 
                               bins = [0, 15, 30, 45, 60, 70, 120],
-                              labels = [0, 15, 30, 45, 60, 70])
+                              labels = ['0 to 15', '15 to 30', '30 to 45',
+                                        '45 to 60', '60 to 70', '70 to 120'])
     df['tax_bin'] = pd.cut(df.tax_rate, 
                               bins = [0, 0.0105, 0.0115, 0.012, 0.0125, 0.015, 0.5],
-                              labels = [0, 0.0105, 0.0115, 0.012, 0.0125, 0.015])
+                              labels = ['0 to 0.0105', '0.0105 to 0.0115', '0.0115 to 0.012',
+                                        '0.012 to 0.0125', '0.0125 to 0.015', '0.015 to 0.5'])
     
     return df
 
@@ -96,6 +100,29 @@ def cluster_features(df, k):
     df['cluster_lot'] = 'cluster_' + df.cluster_lot.astype(str)
 
     df = df.drop(columns=[ 'landtaxvaluedollarcnt'])
+    
+    # Cluster binned features to use them
+    group3 = df[['finishedsquarefeet12', 'age', 'tax_rate']]
+    scaler = MinMaxScaler(copy=True, feature_range=(0,1)).fit(group3)
+    g3_scaled = pd.DataFrame(
+        scaler.transform(group3),
+        columns=group3.columns.values).set_index([group3.index.values])
+    kmeans.fit(g3_scaled)
+    kmeans.predict(g3_scaled)
+    df['cluster_bins'] = kmeans.predict(g3_scaled)
+    
+    # get the centroids for each distinct cluster...
+    cluster_col_name = 'cluster_bins'
+    centroid_col_names = ['centroid_' + i for i in group3]
+    centroids = pd.DataFrame(kmeans.cluster_centers_, columns=centroid_col_names).reset_index()
+    centroids.rename(columns = {'index': cluster_col_name}, inplace = True)
+    
+    # merge the centroids with the df
+    df = df.merge(centroids, how='left',
+                  on=cluster_col_name).set_index(df.index)
+    
+    # Make cluster assignments string
+    df['cluster_bins'] = 'cluster_' + df.cluster_bins.astype(str)
     
     return df
     
